4fdb4bd2d760ebc6cc5e59c1782391958bbbc695
jcarapet <jeffcarapetyan@gmail.com>
2014-03-11 14:24:15 -0500

    adjusted path name correctly

--- recipes/set_fabric8_home.rb
+++ recipes/set_fabric8_home.rb
@@ -10,6 +10,6 @@ file "/etc/profile.d/fabric8.sh" do
 user "root"
 group "root"
 mode "0755"
-content "export CAMEL_HOME=/usr/local/camel/fabric8-karaf-1.0.0.redhat-362"
+content "export FABRIC_HOME=/usr/local/fabric8/fabric8-karaf-1.0.0.redhat-362"
 action :create
 end
